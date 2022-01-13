06688ee69184a278218526013f60237e1188a89b
b06901089 <b06901089@ntu.edu.tw>
2022-01-14 00:54:11 +0800

    add part of game logic

--- frontend/src/components/rock-paper-scissors/SampleImage.js
+++ frontend/src/components/rock-paper-scissors/SampleImage.js
@@ -10,4 +10,16 @@ const SampleImage = {
     },
 };
 
-export default SampleImage;
+const SampleImage1 = {
+    create: () => {
+        let imageEl = new Image(1024, 701);
+        imageEl.src = require("../../img/sample_paper.jpg");
+        return new Promise(resolve => {
+            imageEl.onload = function () {
+                resolve(imageEl);
+            }
+        });
+    },
+};
+
+export { SampleImage, SampleImage1 };
