86e242d9acd2902429d4d3de80f6e08b39d743e7
feidon <r09521515@ntu.edu.tw>
2022-01-16 14:11:00 +0800

    a

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,6 +1,6 @@
 const Query = {
   users: async (parent, args, { userModel }) => {
-    const users = await userModel.find().limit(30);
+    const users = await userModel.find({}, "name scores").limit(30);
     return users;
   },
 };
